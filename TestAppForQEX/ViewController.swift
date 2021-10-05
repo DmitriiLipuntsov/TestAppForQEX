e44c211f7d386cb10de5b3d6673866b8dd2a7808
DmitriiLipuntsov <DmitriiLipuntsov@yandex.ru>
2021-10-05 16:28:39 +0500

    Initial commit

---
+++ TestAppForQEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppForQEX
+//
+//  Created by Михаил Липунцов on 05.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
